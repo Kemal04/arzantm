4768fcee95ab728eecac1a0f1e3114e6d6b4a738
Akynyaz64 <mr.akynyaz29@gmail.com>
2023-06-16 08:53:51 +0500

    init first admin pages

---
+++ src/pages/admin/index.js
@@ -0,0 +1,4 @@
+export {default as Admin} from "./main/Admin";
+export {default as AdminUsers} from "./users/Users";
+export {default as AdminBanners} from "./banners/Banners";
+export {default as AdminLogin} from "./auth/Login";
