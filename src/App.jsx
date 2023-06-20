d5a802cd95b5c03e026f2e6c92d3c85de04bba63
Akynyaz64 <mr.akynyaz29@gmail.com>
2023-06-20 12:28:23 +0500

    auth and context

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import {BrowserRouter as Router, Outlet, Route, Routes} from "react-router-dom";
+import {Toaster} from "react-hot-toast";
 
 //COMPONENTS
 import {Footer, Navbar, ScrollToTop} from "./components";
@@ -11,88 +12,91 @@ import AdminLayout from "./pages/admin/Layout";
 import {Admin, AdminBannerCreate, AdminBanners, AdminCategories, AdminCategoryCreate, AdminComments, AdminLogin, AdminNotifications, AdminPages, AdminPayments, AdminPhotoCreate, AdminPhotos, AdminPostCreate, AdminPosts, AdminSubCategories, AdminSubCategoryCreate, AdminUserCreate, AdminUsers, AdminVideoCreate, AdminVideos, AdminWelayats} from "./pages/admin";
 
 //OTHERS
-import {ToastContainer} from "react-toastify";
+import {AuthProvider} from "./context/AuthContext";
 import ThemeContextProvider from "./context/ThemeContext";
+import NotFound from "./pages/NotFound";
 import "./App.css";
 
 const App = () => {
     return (
         <>
-            {/* <AuthContext.Provider value={{ authState, setAuthState }}> */}
-            <ThemeContextProvider>
-                <Router>
-                    <ScrollToTop />
-                    <ToastContainer />
-                    <Routes>
-                        <Route path="/" element={<HomeLayout />}>
-                            <Route path="/" element={<Home />} />
+            <AuthProvider>
+                <ThemeContextProvider>
+                    <Router>
+                        <ScrollToTop />
+                        <Toaster />
+                        <Routes>
+                            <Route path="/" element={<HomeLayout />}>
+                                <Route path="/" element={<Home />} />
 
-                            <Route path="/foto" element={<Foto />} />
-                            <Route path="/foto/arzanTm" element={<FotoRead />} />
+                                <Route path="/foto" element={<Foto />} />
+                                <Route path="/foto/arzanTm" element={<FotoRead />} />
 
-                            <Route path="/video" element={<Video />} />
+                                <Route path="/video" element={<Video />} />
 
-                            <Route path="/top-list" element={<TopList />} />
+                                <Route path="/top-list" element={<TopList />} />
 
-                            <Route path="/profile" element={<Profile />} />
-                            <Route path="/profile/wallet" element={<ProfileWallet />} />
-                            <Route path="/profile/bloked" element={<ProfileBloked />} />
+                                <Route path="/profile" element={<Profile />} />
+                                <Route path="/profile/wallet" element={<ProfileWallet />} />
+                                <Route path="/profile/bloked" element={<ProfileBloked />} />
 
-                            <Route path="/offical" element={<Offical />} />
-                            <Route path="/offical/follow" element={<OfficalFollow />} />
-                            <Route path="/offical/expired" element={<OfficalExpired />} />
-                            <Route path="/offical/self" element={<OfficalSelf />} />
+                                <Route path="/offical" element={<Offical />} />
+                                <Route path="/offical/follow" element={<OfficalFollow />} />
+                                <Route path="/offical/expired" element={<OfficalExpired />} />
+                                <Route path="/offical/self" element={<OfficalSelf />} />
 
-                            <Route path="/post-gosmak" element={<PostAdd />} />
+                                <Route path="/post-gosmak" element={<PostAdd />} />
 
-                            <Route path="/bildirisler" element={<Notifications />} />
-                            <Route path="/bildiris/:notificationId" element={<NotificationRead />} />
+                                <Route path="/bildirisler" element={<Notifications />} />
+                                <Route path="/bildiris/:notificationId" element={<NotificationRead />} />
 
-                            <Route path="/habarnamalar" element={<Notices />} />
-                            <Route path="/habarnama/:noticeId" element={<NoticeRead />} />
-                            <Route path="/habarnama-gos" element={<NoticeCreate />} />
-                        </Route>
+                                <Route path="/habarnamalar" element={<Notices />} />
+                                <Route path="/habarnama/:noticeId" element={<NoticeRead />} />
+                                <Route path="/habarnama-gos" element={<NoticeCreate />} />
+                            </Route>
 
-                        <Route path="/admin" element={<AdminLayout />}>
-                            <Route path="" element={<Admin />} />
+                            <Route path="/admin" element={<AdminLayout />}>
+                                <Route path="" element={<Admin />} />
 
-                            <Route path="welayats" element={<AdminWelayats />} />
+                                <Route path="welayats" element={<AdminWelayats />} />
 
-                            <Route path="users" element={<AdminUsers />} />
-                            <Route path='user-create' element={<AdminUserCreate />} />
+                                <Route path="users" element={<AdminUsers />} />
+                                <Route path="user-create" element={<AdminUserCreate />} />
 
-                            <Route path="banners" element={<AdminBanners />} />
-                            <Route path="banner-create" element={<AdminBannerCreate />} />
+                                <Route path="banners" element={<AdminBanners />} />
+                                <Route path="banner-create" element={<AdminBannerCreate />} />
 
-                            <Route path="categories" element={<AdminCategories />} />
-                            <Route path="category-create" element={<AdminCategoryCreate />} />
+                                <Route path="categories" element={<AdminCategories />} />
+                                <Route path="category-create" element={<AdminCategoryCreate />} />
 
-                            <Route path="sub-categories" element={<AdminSubCategories />} />
-                            <Route path="sub-category-create" element={<AdminSubCategoryCreate />} />
+                                <Route path="sub-categories" element={<AdminSubCategories />} />
+                                <Route path="sub-category-create" element={<AdminSubCategoryCreate />} />
 
-                            <Route path="comments" element={<AdminComments />} />
+                                <Route path="comments" element={<AdminComments />} />
 
-                            <Route path="gallery/photos" element={<AdminPhotos />} />
-                            <Route path="gallery/photo-create" element={<AdminPhotoCreate />} />
+                                <Route path="gallery/photos" element={<AdminPhotos />} />
+                                <Route path="gallery/photo-create" element={<AdminPhotoCreate />} />
 
-                            <Route path="gallery/videos" element={<AdminVideos />} />
-                            <Route path="gallery/video-create" element={<AdminVideoCreate />} />
+                                <Route path="gallery/videos" element={<AdminVideos />} />
+                                <Route path="gallery/video-create" element={<AdminVideoCreate />} />
 
-                            <Route path="notifications" element={<AdminNotifications />} />
+                                <Route path="notifications" element={<AdminNotifications />} />
 
-                            <Route path="pages" element={<AdminPages />} />
+                                <Route path="pages" element={<AdminPages />} />
 
-                            <Route path="payments" element={<AdminPayments />} />
+                                <Route path="payments" element={<AdminPayments />} />
 
-                            <Route path="posts" element={<AdminPosts />} />
-                            <Route path="post-create" element={<AdminPostCreate />} />
-                        </Route>
+                                <Route path="posts" element={<AdminPosts />} />
+                                <Route path="post-create" element={<AdminPostCreate />} />
+                            </Route>
 
-                        <Route path="/admin/login" element={<AdminLogin />} />
-                    </Routes>
-                </Router>
-            </ThemeContextProvider>
-            {/* </AuthContext.Provider> */}
+                            <Route path="/admin/login" element={<AdminLogin />} />
+
+                            <Route path="*" element={<NotFound />} />
+                        </Routes>
+                    </Router>
+                </ThemeContextProvider>
+            </AuthProvider>
         </>
     );
 };
--- src/pages/admin/auth/Login.jsx
+++ src/pages/admin/auth/Login.jsx
@@ -1,6 +1,57 @@
-import login_img from '../../../assets/auth/login.png'
+import {useRef, useState} from "react";
+import {useNavigate} from "react-router-dom";
+import {toast} from "react-hot-toast";
+import {useAuth} from "../../../context/AuthContext";
+import login_img from "../../../assets/auth/login.png";
 
 const Login = () => {
+    const {setAdmin, setIsLoggedIn} = useAuth();
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const navigate = useNavigate();
+    const email = useRef("");
+    const password = useRef("");
+
+    const submitHandler = async (e) => {
+        setIsSubmitting(true);
+        e.preventDefault();
+        const loginData = {
+            email: email.current.value,
+            password: password.current.value,
+        };
+        const response = await fetch(`${import.meta.env.VITE_API_FETCH_LOCAL}admin/account/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(loginData),
+        });
+
+        const resData = await response.json();
+        console.log(resData);
+        if (resData.status === false) {
+            toast.error(resData.message);
+            setIsSubmitting(false);
+            return null;
+        }
+
+        if (resData.status === true) {
+            toast.success(resData.message);
+            setIsSubmitting(false);
+            const token = resData.data.token;
+            localStorage.setItem("adACto", token);
+
+            setIsLoggedIn(true);
+            setAdmin({
+                id: 1,
+                role: "Admin",
+                status: true,
+            });
+
+            navigate("/admin");
+        }
+
+        setIsSubmitting(false);
+    };
     return (
         <>
             <section className="login-content">
@@ -13,23 +64,23 @@ const Login = () => {
                                         <div className="p-3">
                                             <h2 className="fw-bold">Ulgama gir</h2>
                                             <p className="my-4 text-secondary">Administrator paneline girmek.</p>
-                                            <form>
+                                            <form onSubmit={submitHandler}>
                                                 <div className="row">
                                                     <div className="col-lg-12">
                                                         <div className="floating-label form-group">
-                                                            <input className="floating-input form-control" type="email" placeholder=" " />
+                                                            <input className="floating-input form-control" id="email" type="email" name="email" placeholder="" ref={email} required />
                                                             <label>Email</label>
                                                         </div>
                                                     </div>
                                                     <div className="col-lg-12">
                                                         <div className="floating-label form-group">
-                                                            <input className="floating-input form-control" type="password" placeholder=" " />
+                                                            <input className="floating-input form-control" id="password" type="password" name="password" placeholder=" " ref={password} required />
                                                             <label>Parol</label>
                                                         </div>
                                                     </div>
                                                 </div>
-                                                <button type="submit" className="btn btn-primary rounded-3 ">
-                                                    Ulgama gir
+                                                <button type="submit" className="btn btn-primary rounded-3 " disabled={isSubmitting}>
+                                                    {isSubmitting ? "Tassyklanýar..." : "Tassykla"}
                                                 </button>
                                             </form>
                                         </div>
