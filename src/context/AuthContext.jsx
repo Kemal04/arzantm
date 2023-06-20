820b392318723968925cb6550307e4a97824ce8a
Akynyaz64 <mr.akynyaz29@gmail.com>
2023-06-20 12:27:25 +0500

    little pages

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,3 +1,18 @@
-import { createContext } from "react";
+import {createContext, useContext, useState} from "react";
 
-export const AuthContext = createContext("");
+const AuthContext = createContext(null);
+
+export const AuthProvider = (props) => {
+    const [admin, setAdmin] = useState({
+        id: 1,
+        status: true,
+        role: "Admin",
+    });
+    const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+    return <AuthContext.Provider value={{admin, setAdmin, isLoggedIn, setIsLoggedIn}}>{props.children}</AuthContext.Provider>;
+};
+
+export const useAuth = () => {
+    return useContext(AuthContext);
+};
